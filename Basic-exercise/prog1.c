78d49d04e553cad76633a733abd24ae378c42588
mdszhossain <shazzadmail1#gmail.com>
2023-12-18 20:54:02 +0600

    first commit

---
+++ Basic-exercise/prog1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int integer_value;
+    float floating_value;
+    double big_floating_value;
+    char character;
+
+    printf("Enter integer_value: ");
+    scanf("%d", &integer_value);
+
+    printf("Enter floating_value: ");
+    scanf("%f", &floating_value);
+
+    printf("Enter big_floating_value: ");
+    scanf("%lf", &big_floating_value);
+
+    printf("Enter character: ");
+    scanf(" %c", &character);
+
+    printf("integer_value: %d\n", integer_value);
+    printf("floating_value: %f\n", floating_value);
+    printf("big_floaging_value: %0.2lf\n", big_floating_value);
+    printf("character: %c\n", character);
+
+    return 0;
+}
